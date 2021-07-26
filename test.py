83f06d82816150c4e56f3e568c673c3bc98fe5b0
hwseol <mhmh2090@gmail.com>
2021-07-26 17:54:02 +0900

    CREON TRADE

---
+++ test.py
@@ -0,0 +1,39 @@
+import requests
+import sys
+from PyQt5.QtWidgets import *
+import win32com.client
+
+objCpCybos = win32com.client.Dispatch("CpUtil.CpCybos")
+bConnect = objCpCybos.IsConnect
+if (bConnect == 0):
+    print("PLUS가 정상적으로 연결되지 않음. ")
+    exit()
+
+# 현재가 객체 구하기
+objStockMst = win32com.client.Dispatch("DsCbo1.StockMst")
+objStockMst.SetInputValue(0, 'A005930')  # 종목 코드 - 삼성전자
+objStockMst.BlockRequest()
+
+# 현재가 통신 및 통신 에러 처리
+rqStatus = objStockMst.GetDibStatus()
+rqRet = objStockMst.GetDibMsg1()
+print("통신상태", rqStatus, rqRet)
+if rqStatus != 0:
+    exit()
+
+offer = objStockMst.GetHeaderValue(16)  # 매도호가
+
+
+
+
+
+def post_message(token, channel, text):
+    response = requests.post("https://slack.com/api/chat.postMessage",
+        headers={"Authorization": "Bearer "+token},
+        data={"channel": channel,"text": text}
+    )
+    print(response)
+ 
+myToken = ""
+ 
+post_message(myToken,"#1",'삼성전자 주식 : ' + str(offer))
